690ecc491a18c9c23101a9a5663e2db07259a24c
PerfectionistAF <ihabsohaila@yahoo.com>
2024-10-03 21:43:31 +0300

    Created sample model

--- Django_App/core/climate_story/models.py
+++ Django_App/core/climate_story/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Emissions(models.Model):
+    entity = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()#####for visualization
+    year = models.IntegerField()
+    electricity_access = models.FloatField()
+    cooking_fuel_access = models.FloatField()
+    renewable_electricity_capacity = models.FloatField()
+    fossil_fuel_electricity = models.FloatField()
+    nuclear_electricity = models.FloatField()
+    renewable_electricity = models.FloatField()
+    low_carbon_electricity = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+
 
