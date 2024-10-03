7407c287f6ddefe27022b13a0d38225008669f8d
PerfectionistAF <ihabsohaila@yahoo.com>
2024-10-03 20:51:46 +0300

    django app added

--- C_Stocks_Dash.py
+++ C_Stocks_Dash.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
+import plotly.express as px   
 
 path = './pilot_topdown_CO2_Budget_countries_v1.csv'
 df_co2 = pd.read_csv(path, skiprows=52)
