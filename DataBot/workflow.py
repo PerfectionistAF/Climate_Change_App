aebab3d4ee6a608a93c1e242bae765abff82d405
MohamedAlyMatar <mohamedalymatar2001@gmail.com>
2024-10-05 02:45:15 +0300

    workflow output is now displayed in the dashboard witha click of a btn - STILL NOT PERFECT FOR USER

--- DataBot/workflow.py
+++ DataBot/workflow.py
@@ -100,23 +100,25 @@ class Writer:
 writer = Writer()
 
 ############################################################################
+''' 
+    un-comment and run to test it
+'''
+# import pandas as pd
 
-import pandas as pd
+# file_path = "../Data/pilot_topdown_CO2_Budget_countries_v1.csv"
+# data = pd.read_csv(file_path)
 
-file_path = "../Data/pilot_topdown_CO2_Budget_countries_v1.csv"
-data = pd.read_csv(file_path)
+# target_country_code = 'USA'
+# country_data = data[data['Alpha 3 Code'] == target_country_code]
 
-target_country_code = 'USA'
-country_data = data[data['Alpha 3 Code'] == target_country_code]
+# if not country_data.empty:
+#     preprocessed_data = country_data.to_string()
+# else:
+#     preprocessed_data = "No data available for the specified country."
 
-if not country_data.empty:
-    preprocessed_data = country_data.to_string()
-else:
-    preprocessed_data = "No data available for the specified country."
-
-initial_state = {
-    'messages': [{'role' : "user", "content" : preprocessed_data}],
-    'revision_times': 0
-}
-output = writer.graph.invoke(initial_state, {'configurable' : {'thread_id' : 1, 'checkpoint_ns' : 0, 'checkpoint_id' : 0}})
-print(output)
+# initial_state = {
+#     'messages': [{'role' : "user", "content" : preprocessed_data}],
+#     'revision_times': 0
+# }
+# output = writer.graph.invoke(initial_state, {'configurable' : {'thread_id' : 1, 'checkpoint_ns' : 0, 'checkpoint_id' : 0}})
+# print(output)
