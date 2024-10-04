67c62a8317bb5ff8d7a7914a53ff2bd29af1e4bd
PerfectionistAF <ihabsohaila@yahoo.com>
2024-10-04 13:23:18 +0300

    Django API finished, integrate streamlit app

--- Django_App/core/climate_story/api/serializers.py
+++ Django_App/core/climate_story/api/serializers.py
@@ -2,7 +2,16 @@
 from rest_framework import serializers
 from ..models import Emissions
 
+class DecimalSerializer(serializers.Field):
+    def round_hundredth(self, obj):
+        return round(obj, 2)
+    
+    def to_internal_value(self, data):
+        return data
+    
 class EmissionsSerializer(serializers.ModelSerializer):
+    created = serializers.SerializerMethodField()  ##need a mthd outside of Meta class to getthe field
+    #latitude = round('latitude', 2)
     class Meta:
         model = Emissions
         fields = (   ##cast fields in a tuple
@@ -20,3 +29,6 @@ class EmissionsSerializer(serializers.ModelSerializer):
             'low_carbon_electricity',
             'created',
         )  ##optimize this later
+
+    def get_created(self, obj):  ##created date object
+        return obj.created.strftime("%Y-%m-%d , %H:%M:%S")
